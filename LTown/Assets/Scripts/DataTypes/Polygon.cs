d374cf55b57570f2fb4b8951cd52a8b7bd57cff2
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-04-16 21:33:34 +0200

    Polygon datatype added.

---
+++ LTown/Assets/Scripts/DataTypes/Polygon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DataTypes
+{
+    public class Polygon
+    {
+        private List<Vec3> Points;
+
+        public Polygon()
+        {
+            this.Points = new List<Vec3>();
+        }
+
+        public void AddPoint(Vec3 Point)
+        {
+            this.Points.Add(Point);
+        }
+
+        public void RemovePoint(Vec3 Point)
+        {
+            this.Points.Remove(Point);
+        }
+    }
+}
