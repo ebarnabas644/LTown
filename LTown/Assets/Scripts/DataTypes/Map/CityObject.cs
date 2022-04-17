7decda8d019847c73bafa82cb33b6ba260c5d56a
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-04-17 22:42:23 +0200

    CityObject data structure for holding GameObject and Position data.

---
+++ LTown/Assets/Scripts/DataTypes/Map/CityObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DataTypes.Map
+{
+    public class CityObject : ILocatable
+    {
+        private GameObject _gameObject;
+        
+        public CityObject(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+        }
+        
+        public GameObject GetGameObject()
+        {
+            return _gameObject;
+        }
+        
+        public Vec3 GetPosition()
+        {
+            Vector3 pos = _gameObject.transform.position;
+            return new Vec3(pos.x, pos.y, pos.z);
+        }
+    }
+}
